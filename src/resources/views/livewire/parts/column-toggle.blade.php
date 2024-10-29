c9696ead6805367bb9e09006e7c7f07ef8acd4f6
fantismic <mpicardsmith@teco.com.ar>
2024-10-29 18:56:32 -0300

    add buttons arrow

--- src/resources/views/livewire/parts/column-toggle.blade.php
+++ src/resources/views/livewire/parts/column-toggle.blade.php
@@ -3,9 +3,24 @@
     <!-- Toggle Button -->
     <button @click="isOpenColumnToggle = ! isOpenColumnToggle" class="outline-none inline-flex justify-center items-center group hover:shadow-sm focus:ring-offset-background-white dark:focus:ring-offset-background-dark transition-all ease-in-out duration-200 focus:ring-2 disabled:opacity-80 disabled:cursor-not-allowed bg-opacity-60 dark:bg-opacity-30 text-secondary-600 bg-secondary-300 dark:bg-secondary-600 dark:text-secondary-400 hover:bg-opacity-60 dark:hover:bg-opacity-30 hover:text-secondary-800 hover:bg-secondary-400 dark:hover:text-secondary-400 dark:hover:bg-secondary-500 focus:bg-opacity-60 dark:focus:bg-opacity-30 focus:ring-offset-2 focus:text-secondary-800 focus:bg-secondary-400 focus:ring-secondary-400 dark:focus:text-secondary-400 dark:focus:bg-secondary-500 dark:focus:ring-secondary-700 rounded-md gap-x-2 text-sm px-4 py-2" type="button">
         {{ucfirst(__('yat::yat.columns'))}}
-        <svg aria-hidden="true" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 size-4 totate-0">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
-        </svg>
+        <div>
+            <svg 
+                aria-hidden="true" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                stroke-width="2" 
+                stroke="currentColor" 
+                class="w-4 h-4 transition-transform duration-300" 
+                :class="!isOpenColumnToggle ? 'rotate-180' : 'rotate-0'"
+            >
+                <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M19.5 15.75l-7.5-7.5-7.5 7.5"
+                />
+            </svg>
+        </div>
     </button>
     <!-- Dropdown Menu -->
     <div x-cloak x-show="isOpenColumnToggle" x-transition @click.outside="isOpenColumnToggle = false" @keydown.down.prevent="$focus.wrap().next()" @keydown.up.prevent="$focus.wrap().previous()" class="shadow-xl min-w-52 z-30 absolute top-11 right-0 inline-block rounded-md whitespace-nowrap" role="menu">

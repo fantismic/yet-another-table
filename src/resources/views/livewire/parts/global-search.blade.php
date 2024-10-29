a20d9b02926724cad4a5122e9df6251326af237f
fantismic <mpicardsmith@teco.com.ar>
2024-10-29 19:01:09 -0300

    fix global search view

--- src/resources/views/livewire/parts/global-search.blade.php
+++ src/resources/views/livewire/parts/global-search.blade.php
@@ -7,7 +7,7 @@
             wire:model.live="search" 
             placeholder="{{ ucfirst(__('yat::yat.search')) }}..." 
             type="text" 
-            class="min-w-96 bg-transparent block w-full border-0 text-gray-900 dark:text-gray-400 p-0 outline-none ring-0 sm:text-sm sm:leading-6 focus:ring-0 focus:border-0 placeholder:text-gray-400 dark:placeholder:text-gray-300 " 
+            class="min-w-[24rem] bg-transparent block w-full border-0 text-gray-900 dark:text-gray-400 p-0 outline-none ring-0 sm:text-sm sm:leading-6 focus:ring-0 focus:border-0 placeholder:text-gray-400 dark:placeholder:text-gray-300 " 
             autocomplete="off"
             id="globalSearch" 
             name="globalSearch"
